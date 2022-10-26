d891a5c957939be77adc2021958342e6a2cd5f31
Phong Vu <nguyennguyen969873@gmail.com>
2022-10-26 11:51:58 +0700

    done

---
+++ script.js
@@ -0,0 +1,46 @@
+const wrapper = document.querySelector(".wrapper");
+form = wrapper.querySelector("form");
+fileInp = wrapper.querySelector("input");
+infoText = form.querySelector("p");
+copyBtn = wrapper.querySelector(".copy");
+closeBtn = wrapper.querySelector(".close");
+
+function fetchRequest(formData, file) {
+  infoText.innerText = "Scanning QR Code ...";
+
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      res = res[0].symbol[0].data;
+      infoText.innerText = res
+        ? "Upload QR Code to Scan"
+        : "Couldn't Scan QR Code";
+      if (!file) return;
+      wrapper.querySelector("textarea").innerText = res;
+      form.querySelector("img").src = URL.createObjectURL(file);
+      wrapper.classList.add("active");
+    })
+    .catch(() => {
+      infoText.innerText = "Couldn't Scan QR Code";
+    });
+}
+
+fileInp.addEventListener("change", (e) => {
+  let file = e.target.files[0];
+  if (!file) return;
+  let formData = new FormData();
+  formData.append("file", file);
+  fetchRequest(formData, file);
+});
+
+copyBtn.addEventListener("click", () => {
+  let text = wrapper.querySelector("textarea").textContent;
+
+  navigator.clipboard.writeText(text);
+});
+
+form.addEventListener("click", () => fileInp.click());
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
